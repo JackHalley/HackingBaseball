467f6a8811262ff015657833cfab3154cd63396c
Zack Adams <imzackadams@protonmail.com>
2023-10-15 14:42:51 -0400

    BROKEN logic error contact form

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 app.secret_key = "some_secret_key"  # Change this to your secret key
 
+
+# TODO: Fix email
 # Configurations for Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'YOUR_EMAIL@gmail.com'
-app.config['MAIL_PASSWORD'] = 'YOUR_PASSWORD'
+app.config['MAIL_USERNAME'] = 'hackingbaseball@gmail.com'
+app.config['MAIL_PASSWORD'] = 'nyfb bqag ekyd tgwr'  # Use the 16-character password you just generated
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
@@ -27,7 +29,7 @@ def contact():
         email = request.form['email']
         message_body = request.form['message']
 
-        msg = Message(subject, sender=email, recipients=['YOUR_EMAIL@gmail.com'])
+        msg = Message(subject, sender=email, recipients=['hackingbaseball@gmail.com'])
         msg.body = message_body
         mail.send(msg)
 
@@ -38,4 +40,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
